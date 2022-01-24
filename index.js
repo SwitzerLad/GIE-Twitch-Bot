a525dca1451923a1eeaa08fb317d6ff2fc5d77be
SwitzerLad <brogamerlp2@gmail.com>
2022-01-24 21:41:49 +0100

    fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const tmi = require('tmi.js'),
-    { channel, username, password } = require('./settings.json');
+const tmi = require("tmi.js"),
+    { channel, username, password } = require("./settings.json");
 
 const options = {
     options: { debug: true },
@@ -17,19 +17,19 @@ const options = {
 const client = new tmi.Client(options);
 client.connect().catch(console.error);
 
-client.on('connected', () => {
-    client.say(channel, 'ImprovMod has connected!')
+client.on("connected", () => {
+    client.say(channel, "ImprovMod has connected!")
 });
 
-client.on('message', (channel, user, message, self) => {
+client.on("message", (channel, user, message, self) => {
     if(self) return;
 
  
-    if(message == '!GIE' || '!gie')  {
-        client.say(channel, 'Thanks for checking us out! If you want to know more about us head over to https://gobsimprovemporium.com/ !');
+    if(message == "!gie")  {
+        client.say(channel, "Thanks for checking us out! If you want to know more about us head over to https://gobsimprovemporium.com");
     }
 });
 
-client.on('disconnected', () => {
+client.on("disconnected", () => {
     client.say(channel, "ImprovMod has disconnected!")
 });
